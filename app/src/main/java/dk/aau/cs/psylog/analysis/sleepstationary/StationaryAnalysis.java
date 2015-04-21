8bc4465c9558748be17a03e90465f560007d81a7
Mathias Winde <Mapede11@student.aau.dk>
2015-04-21 11:23:35 +0200

    Ændret så den kan køre for sig selv.

--- app/src/main/java/dk/aau/cs/psylog/analysis/sleepstationary/StationaryAnalysis.java
+++ app/src/main/java/dk/aau/cs/psylog/analysis/sleepstationary/StationaryAnalysis.java
@@ -1,24 +1,21 @@
 package dk.aau.cs.psylog.analysis.sleepstationary;
 
 import android.content.ContentResolver;
-import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.util.Log;
+import android.util.Pair;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
-import java.util.Queue;
 
 import dk.aau.cs.psylog.module_lib.DBAccessContract;
 import dk.aau.cs.psylog.module_lib.IScheduledTask;
@@ -33,65 +30,75 @@ public class StationaryAnalysis implements IScheduledTask{
     public StationaryAnalysis(Context context)
     {
         contentResolver = context.getContentResolver();
-
     }
+
+    private Pair<Integer,Integer> getLastPos(){
+        Uri uri = Uri.parse(DBAccessContract.DBACCESS_CONTENTPROVIDER + "SLEEPSTATIONARY_state");
+        Cursor cursor = contentResolver.query(uri, new String[]{"accPos", "amplPos"}, null, null, null);
+        if(cursor.moveToFirst())
+        {
+            return new Pair<Integer,Integer>(cursor.getInt(cursor.getColumnIndex("accPos")),cursor.getInt(cursor.getColumnIndex("accPos")));
+        }
+        else
+        {
+            return new Pair<Integer,Integer>(0,0);
+        }
+    }
+
+    int lastPosAcc = 0;
+    private Map<String,Float> loadAcc(int lastPos){
+        Map<String,Float> resultMap = new HashMap<String, Float>();
+        Uri uri = Uri.parse(DBAccessContract.DBACCESS_CONTENTPROVIDER + "ACCELERATIONSLEEPANALYSIS_sleepcalc");
+        Cursor cursor = contentResolver.query(uri, new String[]{"_id", "prob", "time"},"_id >= " + lastPos, null,null);
+        if(cursor.moveToFirst()){
+            do{
+                float prob = cursor.getFloat(cursor.getColumnIndex("prob"));
+                String time = cursor.getString(cursor.getColumnIndex("time"));
+                lastPosAcc = cursor.getInt(cursor.getColumnIndex("_id"));
+                resultMap.put(time, prob);
+            }while (cursor.moveToNext());
+            cursor.close();
+        }
+        return resultMap;
+    }
+
+    int lastPosAmpl = 0;
+    private Map<String,Float> loadAmpl(int lastPos){
+        Map<String,Float> resultMap = new HashMap<String, Float>();
+        Uri uri = Uri.parse(DBAccessContract.DBACCESS_CONTENTPROVIDER + "SOUNDSLEEPANALYSIS_sleepcalc");
+        Cursor cursor = contentResolver.query(uri, new String[]{"_id", "prob", "time"},"_id >= " + lastPos, null,null);
+        if(cursor.moveToFirst()){
+            do{
+                float prob = cursor.getFloat(cursor.getColumnIndex("prob"));
+                String time = cursor.getString(cursor.getColumnIndex("time"));
+                lastPosAmpl = cursor.getInt(cursor.getColumnIndex("_id"));
+                resultMap.put(time, prob);
+            }while (cursor.moveToNext());
+            cursor.close();
+        }
+        return resultMap;
+    }
+
     public void Analyse()
     {
-        Log.i("Analyse soevn", "analysen startet");
-        AccelerationSleepAnalysis accelerationSleepAnalysis = new AccelerationSleepAnalysis(contentResolver);
-        AmplitudeSleepAnalysis amplitudeSleepAnalysis = new AmplitudeSleepAnalysis(contentResolver);
-        Map<String, Float> resAcc = accelerationSleepAnalysis.Analyse();
-        Log.i("Analyse soevn", "acc analyse færdig");
-        Map<String, Float> resAmpl = amplitudeSleepAnalysis.Analyse();
-        Log.i("Analyse soevn", "ampl analyse færdig");
-        if(resAcc ==  null && resAmpl == null)
-            return;
-        else if(resAcc == null)
-        {
-            float accProb = readProb("probAcc");
-            for(Map.Entry<String, Float> entry : resAmpl.entrySet())
-            {
-                reportState(entry.getValue()*0.7f + 0.3f*accProb, entry.getKey());
-            }
-            return;
-        }
-        else if(resAmpl == null)
-        {
-            float amplProb = readProb("probAmpl");
-            for(Map.Entry<String, Float> entry : resAcc.entrySet())
-            {
-                reportState(entry.getValue()*0.3f + amplProb*0.7f, entry.getKey());
-            }
-            return;
-        }
+        Pair<Integer,Integer> accAmplLastPos = getLastPos();
+        Map<String, Float> resAcc = loadAcc(accAmplLastPos.first);
+        Map<String, Float> resAmpl = loadAmpl(accAmplLastPos.first);
 
         try {
             //kombinering
             Map<String, Float> result = weightedAverage(resAcc, resAmpl);
             for(Map.Entry<String, Float> entry : result.entrySet())
             {
-                reportState(entry.getValue(), entry.getKey());
+                insertData(entry.getValue(), entry.getKey());
             }
 
         }
         catch (ParseException e){Log.e("ERROR", e.getMessage());}
 
-
+        reportState();
     }
 
-    private float readProb(String column)
-    {
-        Uri uri = Uri.parse(DBAccessContract.DBACCESS_CONTENTPROVIDER + "SLEEPSTATIONARY_state");
-        Cursor cursor = contentResolver.query(uri, new String[]{column}, null, null, null);
-        if(cursor.moveToFirst())
-        {
-            return cursor.getFloat(cursor.getColumnIndex(column));
-        }
-        else
-        {
-            return 0.0f;
-        }
-    }
     private Map<String, Float> weightedAverage(Map<String, Float> map1, Map<String,Float> map2) throws ParseException
     {
         LinkedHashMap<String, Float> resultMap = new LinkedHashMap<String, Float>();
@@ -99,7 +106,7 @@ public class StationaryAnalysis implements IScheduledTask{
         Iterator<Map.Entry<String, Float>> it2 = map2.entrySet().iterator();
         Map.Entry<String, Float> ele1 = it1.next();
         Map.Entry<String, Float> ele2 = it2.next();
-        SimpleDateFormat sdf  = new SimpleDateFormat("yyyy-MM-dd kk:mm:ss.SSS");
+        SimpleDateFormat sdf  = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
         while(it1.hasNext() && it2.hasNext())
         {
             float combination = 0.3f*ele1.getValue() + 0.7f*ele2.getValue();
@@ -138,7 +145,7 @@ public class StationaryAnalysis implements IScheduledTask{
         return resultMap;
     }
 
-    private void reportState(float probability, String time)
+    private void insertData(float probability, String time)
     {
         Uri uri = Uri.parse(DBAccessContract.DBACCESS_CONTENTPROVIDER + "SLEEPSTATIONARY_sleepcalc");
         ContentValues values = new ContentValues();
@@ -147,10 +154,18 @@ public class StationaryAnalysis implements IScheduledTask{
         contentResolver.insert(uri,values);
     }
 
+    private void reportState(){
+        Uri uri = Uri.parse(DBAccessContract.DBACCESS_CONTENTPROVIDER + "SLEEPSTATIONARY_state");
+        ContentValues values = new ContentValues();
+        values.put("accPos", lastPosAcc);
+        values.put("amplPos", lastPosAmpl);
+        contentResolver.insert(uri, values);
+    }
 
 
     @Override
     public void doTask() {
+        Log.i("Analyse soevn", "analysen startet");
         Analyse();
         Log.i("Analyse soevn", "analysen færdig");
     }
